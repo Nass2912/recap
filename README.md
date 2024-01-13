bdb5212e379286ad621940a83666442b7d8c406a
nass <soobratty.as@gmail.com>
2024-01-13 17:51:38 +0400

    recap

---
+++ README.md
@@ -0,0 +1,13 @@
+<!-- Creating a restaurant application -->
+
+Step 1
+  Create Model
+
+Step 2
+ routes => controller => view
+
+  See all restaurants
+  See details about one restaurant
+  Create a restaurant
+  Update a restaurant
+  Destroy a restaurant
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
+  resources :restaurants
+
+  # get "/restaurants", to: "restaurants#index"
+  # get "/restaurants/new", to: "restaurants#new"
+  # post "/restaurants", to: "restaurants#create"
+  # get "/restaurants/:id/edit", to: "restaurants#edit"
+  # get "/restaurants/:id", to: "restaurants#show", as: :show_restaurant
+  # delete "restaurants/:id", to: "restaurants#destroy", as: :delete_restaurant
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
